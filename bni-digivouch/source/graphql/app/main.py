f9cad045d0424d3f2c14e13e70f968640a036a44
cleveresta <33019817+cleveresta@users.noreply.github.com>
2021-05-24 20:33:59 +0700

    Add files via upload

--- bni-digivouch/source/graphql/app/main.py
+++ bni-digivouch/source/graphql/app/main.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 from data import db, setup
 import tinydb
-import request,json
+import requests,json,aiohttp
+from string import Template
 
 setup()
 
 class Product(graphene.ObjectType):
     category = graphene.String(default_value="")
-    brand = graphene.String()
+    brand = graphene.String(default_value="")
     product_name = graphene.String()
     product_code = graphene.String()
     paid_price_by_customer = graphene.String()
@@ -22,16 +23,15 @@ class Product(graphene.ObjectType):
     def resolve_category(parent, info):
         return parent.category
 
-
-class Request(graphene.ObjectType):
-    post_quotation = """{"partnerId":"DKfMyNbLtjsP"}"""
-
-    r = requests.post('http://dev.openapi.ayopop.id/api/v1/partner/products', json=json.loads(post_quotation))
-
 class Inquiry(graphene.ObjectType):
-    partner_id = graphene.String()
+    partner_id = graphene.String(default_value="")
     account_number = graphene.String()
+    zone_id = graphene.String()
     product_code = graphene.String()
+    success = graphene.String()
+    respond_code = graphene.String()
+    message = graphene.String()
+    data = graphene.String()
     
 
 class Query(graphene.ObjectType):
@@ -39,7 +39,11 @@ class Query(graphene.ObjectType):
     		startIndex=graphene.Int(default_value=0), endIndex=graphene.Int(default_value=1000) )
     digital_product = graphene.Field(Product, productCode=graphene.String())
     total_count = graphene.Int()
-    inquiry = graphene.Field(Inquiry)
+    inquiry = graphene.Field(Inquiry, partner_id=graphene.String(default_value="*"),
+            account_number=graphene.String(), zone_id=graphene.String(),
+            product_code=graphene.String())
+    product_list_brand = graphene.List(Product, brand=graphene.String(default_value="*"),
+            startIndex=graphene.Int(default_value=0), endIndex=graphene.Int(default_value=1000))
     
     def resolve_total_count(self, info):
     	return len(db)
@@ -76,6 +80,46 @@ class Query(graphene.ObjectType):
                     region_code=cari[0].get("RegionCode"),
                     fee=cari[0].get("Fee"))
     	return p
-    def resolve_inquiry
+    def resolve_product_list_brand(self, info, brand, startIndex, endIndex):
+        produk = tinydb.Query()
+        list_product = []
+        cari = db.all()[startIndex: endIndex] if brand=="*" else db.search(produk.Brand == brand)[startIndex: endIndex]
+        
+        for i in cari:
+            list_product.append(Product(category=i.get("Category"),
+                                    brand=i.get("Brand"),
+    								product_name=i.get("ProductName"),
+    								product_code=i.get("ProductCode"),
+    								paid_price_by_customer=i.get("PaidPriceByCustomer"),
+    								status=i.get("Status"),
+                                    host_partner=i.get("HostPartner"),
+                                    biller_code=i.get("BillerCode"),
+                                    region_code=i.get("RegionCode"),
+                                    fee=i.get("Fee")
+    								))
+        return list_product
+    def resolve_inquiry(self, info, partner_id, account_number, zone_id, product_code):
+        partner = partner_id
+        account = account_number
+        zone = zone_id
+        product = product_code
+        payload = """{"partnerId": "${partner}", 
+            "accountNumber": "${account}", 
+            "zoneId": "${zone}",
+            "productCode": "${product}" }"""
+        
+        t = Template(payload)
+        p = t.substitute(partner=partner, account=account, zone=zone, product=product)
+        
+        data_payload = json.loads(p)
+        
+        r = requests.post('http://dev.openapi.ayopop.id/api/v1/bill/payment', json=data_payload)
+        api_response = r.text
+        print(r.text)
+        
+        respond = json.loads(api_response)
+        
+        return respond
+
 app = FastAPI()
 app.add_route("/graphql", GraphQLApp(schema=graphene.Schema(query=Query)))
